8bbb05e4eaa490a4def36bcecef708bcabeacbd2
cwmcbrewster <43007934+cwmcbrewster@users.noreply.github.com>
2019-10-18 15:35:27 -0700

    Update Computer_Enrollment_LaunchAgent.sh

--- Computer_Enrollment_LaunchAgent.sh
+++ Computer_Enrollment_LaunchAgent.sh
@@ -1,91 +1,161 @@
 #!/bin/bash
 
+# Variables
+
+launchAgentName='org.my.jamfHelperSplashScreen'
+launchAgentPath="/Library/LaunchAgents/${launchAgentName}.plist"
+
+jamfHelperPath='/Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper'
+jamfHelperHeading='My Org'
+jamfHelperIconPath='/Library/Application\ Support/MyOrg/Logo.png'
+
+# Functions
+
+startSplashScreen () {
+
+# Check for user not logged in
+if [[ -z "$loggedInUser" ]]; then
+  
+  # Remove existing LaunchAgent
+  if [[ -f ${launchAgentPath} ]]; then
+    rm ${launchAgentPath}
+  fi
+
+  # Write LaunchAgent to load jamfHelper script
+  defaults write ${launchAgentPath} KeepAlive -bool true
+  defaults write ${launchAgentPath} Label ${launchAgentName}
+  defaults write ${launchAgentPath} LimitLoadToSessionType "LoginWindow"
+  defaults write ${launchAgentPath} ProgramArguments -array-add "$jamfHelperPath"
+  defaults write ${launchAgentPath} ProgramArguments -array-add "-windowType"
+  defaults write ${launchAgentPath} ProgramArguments -array-add "fs"
+  defaults write ${launchAgentPath} ProgramArguments -array-add "-heading"
+  defaults write ${launchAgentPath} ProgramArguments -array-add "$jamfHelperHeading"
+  defaults write ${launchAgentPath} ProgramArguments -array-add "-description"
+  defaults write ${launchAgentPath} ProgramArguments -array-add "$message"
+  defaults write ${launchAgentPath} ProgramArguments -array-add "-icon"
+  defaults write ${launchAgentPath} ProgramArguments -array-add "$jamfHelperIconPath"
+  defaults write ${launchAgentPath} RunAtLoad -bool true 
+  chown root:wheel ${launchAgentPath}
+  chmod 644 ${launchAgentPath}
+  echo "Created Launch Agent to run jamfHelper"
+  
+  # Kill/restart the loginwindow process to load the LaunchAgent
+  echo "Ready to lock screen. Restarting loginwindow..."
+  if [[ ${osvers} -le 14 ]]; then
+    killall loginwindow
+  fi
+  if [[ ${osvers} -ge 15 ]]; then
+    killall loginwindow
+    killall loginwindow # macOS 10.15.0 needs a second login window restart for jamfHelper to stay present
+    #launchctl kickstart -k system/com.apple.loginwindow # kickstarting the login window works but is slower and results in runaway securityAgent processes in macOS 10.15.0
+  fi
+fi
+}
+
+killSplashScreen () {
+# Remove existing LaunchAgent and restart login window
+if [[ -f ${launchAgentPath} ]]; then
+  echo "Removing LaunchAgent located at ${launchAgentPath}"
+  rm ${launchAgentPath}
+fi
+
+echo "Restarting loginwindow..."
+killall loginwindow
+}
+
+removeLaunchAgentAtReboot () {
+# Create a self-destructing LaunchDaemon to remove our LaunchAgent at next startup
+if [[ -f ${launchAgentPath} ]]; then
+  launchDaemonName="${launchAgentName}.remove"
+  launchDaemonPath="/Library/LaunchDaemons/${launchDaemonName}.plist"
+  defaults write ${launchDaemonPath} Label "${launchDaemonName}"
+  defaults write ${launchDaemonPath} ProgramArguments -array-add "rm"
+  defaults write ${launchDaemonPath} ProgramArguments -array-add "${launchAgentPath}"
+  defaults write ${launchDaemonPath} ProgramArguments -array-add "${launchDaemonPath}"
+  defaults write ${launchDaemonPath} RunAtLoad -bool true
+  chown root:wheel ${launchDaemonPath}
+  chmod 644 ${launchDaemonPath}
+  echo "Created Launch Daemon to remove ${launchAgentPath}"
+fi
+}
+
+# Start script
+
 osvers=$(sw_vers -productVersion | awk -F. '{print $2}')
 
-#Cleanup files from prior enrollment
-if [ -f /private/tmp/splash_screen.sh ];then
-  echo "Removing /private/tmp/splash_screen.sh"
-  rm /private/tmp/splash_screen.sh
-fi
-if [ -f /Library/LaunchAgents/ORG.computer_setup.plist ];then
-  echo "Removing /Library/LaunchAgents/ORG.computer_setup.plist"
-  rm /Library/LaunchAgents/ORG.computer_setup.plist
+# Only proceed if macOS version is 10.13 or higer
+if [[ ${osvers} -le 12 ]]; then
+  echo "macOS version 10.$osvers not supported."
+  exit 0
 fi
 
-#Get currently logged in user
-loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
+# Get currently logged in user
+loggedInUser=$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
 
-#Wait for _mbsetupuser to not be logged in (used by Apple for setup screens)
+# Wait for _mbsetupuser to not be logged in (used by Apple for setup screens)
 while [[ $loggedInUser = "_mbsetupuser" ]]
 do
   sleep 5
-  loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
+  loggedInUser=$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
   #echo "Waiting for _mbsetupuser"
 done
 
-#Check for logged in user and exit if true (10.12+)
-if [[ ${osvers} -ge 12 ]];then
-  if [[ -n "$loggedInUser" ]];then
-    echo "$loggedInUser is logged in. Exiting..."
-    exit 0
-  fi
+# Check for logged in user and exit if true
+if [[ -n "$loggedInUser" ]]; then
+  echo "$loggedInUser is logged in. Exiting..."
+  exit 0
 fi
 
-#Check if logged in user is admin and continue if true (10.11)
-if [[ ${osvers} -eq 11 ]];then
-  if [[ $loggedInUser = "admin" ]];then
-    /Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType fs -heading "My Organization" -description "Finishing Setup..." -icon "/private/tmp/ORG_Logo.png" &
-  else
-    echo "Logged in user is not admin. Exiting..."
-    exit 0
-  fi
-fi
+message="Starting Final Setup..."
+startSplashScreen
 
-#Check for user not logged in
-if [ -z "$loggedInUser" ];then
-  #Write jamfHelper splash screen script
-  echo "#!/bin/bash" >> /private/tmp/splash_screen.sh
-  echo "\"/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper\" -windowType fs -heading \"My Organization\" -description \"Finishing Setup...\" -icon \"/private/tmp/ORG_Logo.png\"" >> /private/tmp/splash_screen.sh
-  chmod +x /private/tmp/splash_screen.sh
+# Keep this Mac from dozing off
+caffeinate -d -i -s -t 7200 &
 
-  #Write LaunchAgent to load jamfHelper script
-  defaults write /Library/LaunchAgents/ORG.computer_setup.plist Label "ORG.computer_setup"
-  defaults write /Library/LaunchAgents/ORG.computer_setup.plist LimitLoadToSessionType "LoginWindow"
-  defaults write /Library/LaunchAgents/ORG.computer_setup.plist ProgramArguments -array
-  defaults write /Library/LaunchAgents/ORG.computer_setup.plist KeepAlive -bool true
-  defaults write /Library/LaunchAgents/ORG.computer_setup.plist RunAtLoad -bool true
-  /usr/libexec/PlistBuddy -c "Add ProgramArguments: string /private/tmp/splash_screen.sh" /Library/LaunchAgents/ORG.computer_setup.plist
+# Prevent Jamf check-in policies from running until next reboot
+launchctl unload /Library/LaunchDaemons/com.jamfsoftware.task.1.plist
+launchctl unload /Library/LaunchDaemons/com.jamfsoftware.jamf.daemon.plist
 
-  chown root:wheel  /Library/LaunchAgents/ORG.computer_setup.plist
-  chmod 644 /Library/LaunchAgents/ORG.computer_setup.plist
-  echo "Created Launch Agent to run jamfHelper"
+# Run Jamf enrollment policies (custom these as needed for your environment)
+# When you want to change the jamfHeper message, set the message variable and run startSplashScreen
+# Either run killSplashScreen at the end of your script or use removeLaunchAgentAtReboot if you will be restarting the computer
 
-  #Kill/restart the loginwindow process to load the LaunchAgent
-  echo "Ready to lock screen. Restarting loginwindow process..."
-  kill -9 $(ps axc | awk '/loginwindow/{print $1}')
-fi
-
-#Disable local admin account
-id -u admin
-if [ $? -eq 0 ];then
- echo "admin account exists. disabling..."
- pwpolicy -u admin -disableuser
-fi
-
-#Run enrollment policies
+# Set computer name / join AD
 jamf policy -event enrollment_02
+
+# Enable SSH
 jamf policy -event enrollment_03
+
+# Set Energy Saver
 jamf policy -event enrollment_04
+
+message="Installing Canon Print Drivers..."
+startSplashScreen
 jamf policy -event enrollment_05
+
+message="Installing HP Print Drivers..."
+startSplashScreen
 jamf policy -event enrollment_06
+
+message="Installing Microsoft Office..."
+startSplashScreen
 jamf policy -event enrollment_07
 
-#Enable local admin account
-id -u admin
-if [ $? -eq 0 ];then
- echo "admin account exists. enabling..."
- pwpolicy -u admin -enableuser
-fi
+# Update inventory to avoid running unneccessary startup policies
+message="Updating Inventory..."
+startSplashScreen
+jamf recon
 
-#Last enrollment policy (cleanup and reboot)
+# Run Jamf startup policies
+message="Checking Policies..."
+startSplashScreen
+jamf policy -event startup
+
+# Cleanup (anything you might want to do before starting software updates and/or restarting the computer)
+jamf policy -event enrollment_15
+removeLaunchAgentAtReboot
+
+# Check for software updates and reboot
+message="Checking Software Updates..."
+startSplashScreen
 jamf policy -event enrollment_20
