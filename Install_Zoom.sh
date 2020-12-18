054065e77636ae8b12810d4b063f1a93b12f0c7f
cwmcbrewster <43007934+cwmcbrewster@users.noreply.github.com>
2020-12-18 10:54:07 -0800

    Create Install_Zoom.sh

---
+++ Install_Zoom.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# Automatically download and install the latest zoom.us
+
+# Variables
+appName="zoom.us.app"
+appProcessName="zoom.us"
+downloadUrl="https://zoom.us/client/latest"
+pkgName="ZoomInstallerIT.pkg"
+
+tmpDir=$(mktemp -d)
+echo "Temp dir set to ${tmpDir}"
+
+function processCheck {
+  if [[ -n $(pgrep -x "${appProcessName}") ]]; then
+    echo "${appProcessName} is currently running"
+    echo "Aborting install"
+    exit 0
+  else
+    echo "${appProcessName} not currently running"
+  fi
+}
+
+function tryDownload {
+  curl -LSs ${downloadUrl}/${pkgName} -o "${tmpDir}/${pkgName}"
+}
+
+function versionCheck {
+  appPath="/Applications/${appName}"
+
+  if [[ -d "${appPath}" ]]; then
+    echo "${appName} version is $(defaults read "${appPath}"/Contents/Info.plist CFBundleShortVersionString)"
+    versionCheckStatus=1
+  else
+    echo "${appName} not installed"
+    versionCheckStatus=0
+  fi
+}
+
+# Start
+
+# List version
+versionCheck
+
+# Exit if app is running
+processCheck
+
+# Download PKG file into ${tmpDir}
+tryDownload
+
+# Check curl exit code and try again in 30 seconds if it was not successful
+if [[ ! $? -eq 0 ]]; then
+  echo "Waiting 30 seconds to try again..."
+  sleep 30
+  processCheck
+  tryDownload
+fi
+
+# Check for successful download
+if [[ ! -f "${tmpDir}/${pkgName}" ]]; then
+    echo "Download unsuccessful"
+    exit 1
+fi
+
+# Install package
+installer -pkg "${tmpDir}/${pkgName}" -target /
+
+# Remove downloaded PKG file
+rm -f "${tmpDir}/${pkgName}"
+
+# List version and exit with error code if not found
+versionCheck
+if [[ ${versionCheckStatus} -eq 0 ]]; then
+  exit 1
+fi
