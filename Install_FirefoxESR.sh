1bee5fc0ad927f26d7a82756a713fb7e41026bec
cwmcbrewster <43007934+cwmcbrewster@users.noreply.github.com>
2021-11-10 13:48:56 -0800

    Update Install_FirefoxESR.sh

--- Install_FirefoxESR.sh
+++ Install_FirefoxESR.sh
@@ -3,16 +3,18 @@
 # Automatically download and install the latest Firefox
 
 # Variables
+#currentVersion=$(curl -s https://www.mozilla.org/en-US/firefox/new/ | grep "data-esr-versions" | awk -F"data-esr-versions=" '{print $NF}' | cut -d\" -f 2)
 appName="Firefox.app"
 appProcessName="firefox"
-#downloadUrl="https://download.mozilla.org/?product=firefox-esr-pkg-latest-ssl&os=osx&lang=en-US" # Oldest supported ESR version
-downloadUrl="https://download.mozilla.org/?product=firefox-esr-next-pkg-latest-ssl&os=osx&lang=en-US" # Latest ESR version available
+downloadUrl="https://download.mozilla.org/?product=firefox-esr-pkg-latest-ssl&os=osx&lang=en-US" # Oldest supported ESR version
+#downloadUrl="https://download.mozilla.org/?product=firefox-esr-next-pkg-latest-ssl&os=osx&lang=en-US" # Latest ESR version available
 pkgName=$(curl -sIL "${downloadUrl}" | grep -m 1 "^Location:" | cut -d' ' -f2 | awk -F'/' '{print $NF}' | sed -e 's/%20/ /g' | sed -e 's/[[:space:]]*$//')
+#pkgName="Firefox esr.pkg"
 
 tmpDir=$(mktemp -d)
 echo "Temp dir set to ${tmpDir}"
 
-function processCheck {
+processCheck () {
   if [[ -n $(pgrep -x "${appProcessName}") ]]; then
     echo "${appProcessName} is currently running"
     echo "Aborting install"
@@ -22,11 +24,18 @@ function processCheck {
   fi
 }
 
-function tryDownload {
+tryDownload () {
   curl -LSs "${downloadUrl}" -o "${tmpDir}/${pkgName}"
+  if [[ $? -eq 0 ]]; then
+    echo "Download successful"
+    tryDownloadState=1
+  else
+    echo "Download unsuccessful"
+    tryDownloadCounter=$((tryDownloadCounter+1))
+  fi
 }
 
-function versionCheck {
+versionCheck () {
   appPath="/Applications/${appName}"
 
   if [[ -d "${appPath}" ]]; then
@@ -43,31 +52,28 @@ function versionCheck {
 # List version
 versionCheck
 
-# Exit if app is running
-processCheck
-
-# Download PKG file into tmpDir
-tryDownload
-
-# Check curl exit code and try again in 30 seconds if it was not successful
-if [[ ! $? -eq 0 ]]; then
-  echo "Waiting 30 seconds to try again..."
-  sleep 30
+# Download PKG file into tmpDir (60 second timeouet)
+echo "Starting download"
+tryDownloadState=0
+tryDownloadCounter=0
+while [[ ${tryDownloadState} -eq 0 && ${tryDownloadCounter} -le 60 ]]; do
   processCheck
   tryDownload
-fi
+  sleep 1
+done
 
 # Check for successful download
 if [[ ! -f "${tmpDir}/${pkgName}" ]]; then
-    echo "Download unsuccessful"
-    exit 1
+  echo "Download failed"
+  exit 1
 fi
 
 # Install package
+echo "Starting install"
 installer -pkg "${tmpDir}/${pkgName}" -target /
 
-# Remove downloaded PKG file
-rm -f "${tmpDir}/${pkgName}"
+# Remove tmp dir
+rm -R "${tmpDir}"
 
 # List version and exit with error code if not found
 versionCheck
