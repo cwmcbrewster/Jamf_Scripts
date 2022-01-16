e4307b7c219d3bdb1e2e23de8dbdba10f70a4198
cwmcbrewster <43007934+cwmcbrewster@users.noreply.github.com>
2022-01-16 09:12:29 -0800

    Update Install_FirefoxESR.sh

--- Install_FirefoxESR.sh
+++ Install_FirefoxESR.sh
@@ -1,21 +1,40 @@
-#!/bin/bash
+#!/bin/zsh
 
 # Automatically download and install the latest Firefox
 
 # Variables
 #currentVersion=$(curl -s https://www.mozilla.org/en-US/firefox/new/ | grep "data-esr-versions" | awk -F"data-esr-versions=" '{print $NF}' | cut -d\" -f 2)
 appName="Firefox.app"
+appPath="/Applications/${appName}"
 appProcessName="firefox"
 downloadUrl="https://download.mozilla.org/?product=firefox-esr-pkg-latest-ssl&os=osx&lang=en-US" # Oldest supported ESR version
 #downloadUrl="https://download.mozilla.org/?product=firefox-esr-next-pkg-latest-ssl&os=osx&lang=en-US" # Latest ESR version available
-pkgName=$(curl -sIL "${downloadUrl}" | grep -m 1 "^Location:" | cut -d' ' -f2 | awk -F'/' '{print $NF}' | sed -e 's/%20/ /g' | sed -e 's/[[:space:]]*$//')
-#pkgName="Firefox esr.pkg"
+#curlPkgName="Firefox%20${currentVersion}esr.pkg"
+#pkgName=$(curl -sIL "${downloadUrl}" | grep -m 1 "^Location:" | cut -d' ' -f2 | awk -F'/' '{print $NF}' | sed -e 's/%20/ /g' | sed -e 's/[[:space:]]*$//')
+pkgName="Firefox_esr.pkg"
 
-tmpDir=$(mktemp -d)
-echo "Temp dir set to ${tmpDir}"
+cleanup () {
+  if [[ -f "${tmpDir}/${pkgName}" ]]; then
+    if rm -f "${tmpDir}/${pkgName}"; then
+      echo "Removed file ${tmpDir}/${pkgName}"
+    fi
+  fi
+  if [[ -d "${tmpDir}" ]]; then
+    if rm -R "${tmpDir}"; then
+      echo "Removed directory ${tmpDir}"
+    fi
+  fi
+}
+
+createTmpDir () {
+  if [ -z ${tmpDir+x} ]; then
+    tmpDir=$(mktemp -d)
+    echo "Temp dir set to ${tmpDir}"
+  fi
+}
 
 processCheck () {
-  if [[ -n $(pgrep -x "${appProcessName}") ]]; then
+  if pgrep -x "${appProcessName}" > /dev/null; then
     echo "${appProcessName} is currently running"
     echo "Aborting install"
     exit 0
@@ -25,8 +44,7 @@ processCheck () {
 }
 
 tryDownload () {
-  curl -LSs "${downloadUrl}" -o "${tmpDir}/${pkgName}"
-  if [[ $? -eq 0 ]]; then
+  if curl -LSs "${downloadUrl}" -o "${tmpDir}/${pkgName}"; then
     echo "Download successful"
     tryDownloadState=1
   else
@@ -36,8 +54,6 @@ tryDownload () {
 }
 
 versionCheck () {
-  appPath="/Applications/${appName}"
-
   if [[ -d "${appPath}" ]]; then
     echo "${appName} version is $(defaults read "${appPath}"/Contents/Info.plist CFBundleShortVersionString)"
     versionCheckStatus=1
@@ -52,12 +68,12 @@ versionCheck () {
 # List version
 versionCheck
 
-# Download PKG file into tmpDir (60 second timeouet)
-echo "Starting download"
+# Download pkg file into tmp dir (60 second timeouet)
 tryDownloadState=0
 tryDownloadCounter=0
 while [[ ${tryDownloadState} -eq 0 && ${tryDownloadCounter} -le 60 ]]; do
   processCheck
+  createTmpDir
   tryDownload
   sleep 1
 done
@@ -65,6 +81,7 @@ done
 # Check for successful download
 if [[ ! -f "${tmpDir}/${pkgName}" ]]; then
   echo "Download failed"
+  cleanup
   exit 1
 fi
 
@@ -72,8 +89,8 @@ fi
 echo "Starting install"
 installer -pkg "${tmpDir}/${pkgName}" -target /
 
-# Remove tmp dir
-rm -R "${tmpDir}"
+# Remove tmp dir and downloaded pkg package
+cleanup
 
 # List version and exit with error code if not found
 versionCheck
