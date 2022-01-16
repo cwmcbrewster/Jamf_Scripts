c2e841fb3fa6a6ef493333d41c973218a9694bf1
cwmcbrewster <43007934+cwmcbrewster@users.noreply.github.com>
2022-01-16 09:13:04 -0800

    Update Install_Zoom.sh

--- Install_Zoom.sh
+++ Install_Zoom.sh
@@ -1,35 +1,58 @@
-#!/bin/bash
+#!/bin/zsh
 
 # Automatically download and install the latest zoom.us
 
 # Variables
 appName="zoom.us.app"
+appPath="/Applications/${appName}"
 appProcessName="zoom.us"
 downloadUrl="https://zoom.us/client/latest"
 pkgName="ZoomInstallerIT.pkg"
 
-tmpDir=$(mktemp -d)
-echo "Temp dir set to ${tmpDir}"
+cleanup () {
+  if [[ -f "${tmpDir}/${pkgName}" ]]; then
+    if rm -f "${tmpDir}/${pkgName}"; then
+      echo "Removed file ${tmpDir}/${pkgName}"
+    fi
+  fi
+  if [[ -d "${tmpDir}" ]]; then
+    if rm -R "${tmpDir}"; then
+      echo "Removed directory ${tmpDir}"
+    fi
+  fi
+}
 
-function processCheck {
-  if [[ -n $(pgrep -x "${appProcessName}") ]]; then
+createTmpDir () {
+  if [ -z ${tmpDir+x} ]; then
+    tmpDir=$(mktemp -d)
+    echo "Temp dir set to ${tmpDir}"
+  fi
+}
+
+processCheck () {
+  if pgrep -x "${appProcessName}" > /dev/null; then
     echo "${appProcessName} is currently running"
     echo "Aborting install"
+    cleanup
     exit 0
   else
     echo "${appProcessName} not currently running"
   fi
 }
 
-function tryDownload {
-  curl -LSs ${downloadUrl}/${pkgName} -o "${tmpDir}/${pkgName}"
+tryDownload () {
+  if curl -LSs "${downloadUrl}/${pkgName}" -o "${tmpDir}/${pkgName}"; then
+    echo "Download successful"
+    tryDownloadState=1
+  else
+    echo "Download unsuccessful"
+    tryDownloadCounter=$((tryDownloadCounter+1))
+  fi
 }
 
-function versionCheck {
-  appPath="/Applications/${appName}"
-
+versionCheck () {
   if [[ -d "${appPath}" ]]; then
-    echo "${appName} version is $(defaults read "${appPath}"/Contents/Info.plist CFBundleShortVersionString)"
+    echo "${appName} version is $(defaults read "${appPath}/Contents/Info.plist" CFBundleShortVersionString)"
     versionCheckStatus=1
   else
     echo "${appName} not installed"
@@ -42,31 +65,29 @@ function versionCheck {
 # List version
 versionCheck
 
-# Exit if app is running
-processCheck
-
-# Download PKG file into ${tmpDir}
-tryDownload
-
-# Check curl exit code and try again in 30 seconds if it was not successful
-if [[ ! $? -eq 0 ]]; then
-  echo "Waiting 30 seconds to try again..."
-  sleep 30
+# Download PKG file into ${tmpDir} (60 second timeout)
+tryDownloadState=0
+tryDownloadCounter=0
+while [[ ${tryDownloadState} -eq 0 && ${tryDownloadCounter} -le 60 ]]; do
   processCheck
+  createTmpDir
   tryDownload
-fi
+  sleep 1
+done
 
 # Check for successful download
 if [[ ! -f "${tmpDir}/${pkgName}" ]]; then
     echo "Download unsuccessful"
+    cleanup
     exit 1
 fi
 
 # Install package
+echo "Starting install"
 installer -pkg "${tmpDir}/${pkgName}" -target /
 
-# Remove downloaded PKG file
-rm -f "${tmpDir}/${pkgName}"
+# Remove tmp dir and downloaded pkg file
+cleanup
 
 # List version and exit with error code if not found
 versionCheck
